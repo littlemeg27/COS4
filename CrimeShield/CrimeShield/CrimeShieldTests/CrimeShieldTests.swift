9bdc99ab437f18e37caf19e3945925600183e4e4
Brenna Pavlinchak <littlemeg27@student.fullsail.edu>
2025-01-16 20:28:20 -0500

    Starting

---
+++ CrimeShield/CrimeShield/CrimeShieldTests/CrimeShieldTests.swift
@@ -0,0 +1,17 @@
+//
+//  CrimeShieldTests.swift
+//  CrimeShieldTests
+//
+//  Created by Brenna Pavlinchak on 1/16/25.
+//
+
+import Testing
+@testable import CrimeShield
+
+struct CrimeShieldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ CrimeShield/CrimeShield/CrimeShieldUITests/CrimeShieldUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CrimeShieldUITestsLaunchTests.swift
+//  CrimeShieldUITests
+//
+//  Created by Brenna Pavlinchak on 1/16/25.
+//
+
+import XCTest
+
+final class CrimeShieldUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
